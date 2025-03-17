1bf671e3d11f4cb650b9752b6b3b08a03569e387
Xero <juliuscaesar.caesar37@gmail.com>
2025-03-17 14:46:18 +0700

    Init

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+const test = () => {};
+
+export default function Home() {
+  return (
+    <main className="flex-center screen">
+      {/* Hero */}
+      <section id="hero">
+        <h1>Welcome to my Next.Js Template 2025</h1>
+      </section>
+    </main>
+  );
+}
